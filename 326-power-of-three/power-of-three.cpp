8e6d7e07e7a9696a5047c964780520bb119840c3
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2025-10-21 10:45:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (76.73%) - LeetSync

---
+++ 326-power-of-three/power-of-three.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        const int maxPowerOf3 =
+            1162261467; // 3^19 fits in 32-bit signed int
+        return n > 0 && (maxPowerOf3 % n == 0);
+    }
+
+    // bool isPowerOfThree(int n) {
+    //     if (n <= 0)
+    //         return false;
+    //     while (n % 3 == 0) {
+    //         n /= 3;
+    //     }
+    //     return n == 1;
+    // }
+};
