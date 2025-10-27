d3aca09a5feecf818bd42613dcc266d61e7b493b
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2025-10-27 10:52:10 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (69.59%) - LeetSync

---
+++ Striver_SDE/Day-5/206-reverse-linked-list/reverse-linked-list.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* newHead = nullptr;
+        while(head != nullptr) {
+            ListNode* next = head->next;
+            head->next = newHead;
+            newHead = head;
+            head = next;
+        }
+
+        return newHead;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // see strivers notes for visual rep iterate for a list of 3 node Done and dusted
+        // ListNode* newHead = nullptr;
+        // while(head != nullptr) {
+        //     ListNode* next = head->next;
+        //     head->next = newHead;
+        //     newHead = head;
+        //     head = next;
+        // }
+        
+        // return newHead;
+    }
+};
