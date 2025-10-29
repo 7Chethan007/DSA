097317e4c4a36d7a1628ecd1d1d32295adcb1c00
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2025-10-29 14:11:33 +0530

    Time: 4 ms (8.98%) | Memory: 50.3 MB (33.63%) - LeetSync

---
+++ Striver_SDE/Day-7/485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cnt = 0;
+        int maxCnt = INT_MIN;
+
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == 1){
+                cnt++;
+            }
+            else {
+                cnt = 0;
+            }
+            maxCnt = max(maxCnt, cnt);
+        }
+        return maxCnt;
+    }
+};
