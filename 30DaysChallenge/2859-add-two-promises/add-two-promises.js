03246fbf47fa2ac2849a2dbd537b110840d1d236
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2025-10-30 10:18:22 +0530

    Time: 60 ms (33.72%) | Memory: 55 MB (32.86%) - LeetSync

---
+++ 30DaysChallenge/2859-add-two-promises/add-two-promises.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+  // Wait for both promises to resolve
+  const [val1, val2] = await Promise.all([promise1, promise2]);
+  
+  // Return their sum
+  return val1 + val2;
+};
+
+/**
+ * Example:
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(5))
+ *   .then(console.log); // 7
+ */
