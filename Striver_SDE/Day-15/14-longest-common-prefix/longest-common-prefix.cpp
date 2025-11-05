3af5a039f8b698938372c0393c34b2fc408f0a27
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2025-11-05 12:21:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (95.32%) - LeetSync

--- Striver_SDE/Day-15/14-longest-common-prefix/longest-common-prefix.cpp
+++ Striver_SDE/Day-15/14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& str) {
-        // Handle the case where input is empty
-        if (str.empty())
-            return "";
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
 
-        // Sort the strings lexicographically
-        sort(str.begin(), str.end());
+        string res = "";
 
-        // First string in sorted order
-        string first = str[0];
-
-        // Last string in sorted order
-        string last = str[str.size() - 1];
-
-        // To store the common prefix
-        string ans = "";
-
-        // Compare up to the length of the shorter string
-        int minLength = min(first.size(), last.size());
-
-        // Compare characters of first and last string
-        for (int i = 0; i < minLength; i++) {
-            // Stop if characters differ
-            if (first[i] != last[i])
-                break;
-
-            // Add matching character to answer
-            ans += first[i];
+        for (int i = 0; i < strs[0].size(); ++i) {
+            char ch = strs[0][i];
+            for (int j = 0; j < strs.size(); ++j) {
+                if (i == strs[j].size() || strs[j][i] != ch)
+                    return res;
+            }
+            res += ch;
         }
 
-        // Return the longest common prefix
-        return ans;
+        return res;
     }
 };
