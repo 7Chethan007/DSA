2efffd51ddb7ec729abc1b552e1bb9e932b13a91
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2025-11-06 12:05:17 +0530

    Time: 71 ms (27.00%) | Memory: 55.4 MB (46.36%) - LeetSync

---
+++ Striver_SDE/Day-15/2807-execute-asynchronous-functions-in-parallel/execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,43 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+  return new Promise((resolve, reject) => {
+    const n = functions.length;
+    if (n === 0) return resolve([]);
+
+    const results = new Array(n);
+    let resolvedCount = 0;
+    let rejected = false;
+
+    for (let i = 0; i < n; i++) {
+      let fn = functions[i];
+      let p;
+      try {
+        p = fn(); // may throw synchronously
+      } catch (err) {
+        // synchronous throw -> reject immediately
+        return reject(err);
+      }
+
+      // attach handlers directly to the returned promise
+      p.then((val) => {
+        if (rejected) return;
+        results[i] = val;
+        resolvedCount += 1;
+        if (resolvedCount === n) resolve(results);
+      }).catch((err) => {
+        if (rejected) return;
+        rejected = true;
+        reject(err);
+      });
+    }
+  });
+};
+
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
