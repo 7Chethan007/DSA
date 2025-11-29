277e58aaee297aac78ab907e5162190e31a4c206
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2025-11-29 13:16:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (11.53%) - LeetSync

---
+++ DSA/Strive_SDE/Day-16/367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int start = 0;
+        int end = num;
+
+        while(start <= end) {
+            long mid = start + (end - start) / 2;
+            if (mid * mid == num) {
+                return true;
+            }
+            else if(mid * mid > num) {
+                end = (int) mid - 1;
+            }
+            else if (mid * mid < num) {
+                start = (int) mid + 1;
+            }
+            else 
+            return false;
+        }
+        return false;
+    }
+}
