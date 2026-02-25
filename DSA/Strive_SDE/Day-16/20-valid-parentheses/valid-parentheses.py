bb75f100bd1b62ad7d722de7c6e51967af820533
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2026-02-25 22:25:48 +0530

    Time: 4 ms (12.69%) | Memory: 19.4 MB (24.45%) - LeetSync

---
+++ DSA/Strive_SDE/Day-16/20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        
+        for c in s:
+            # Push opening brackets
+            if c in "({[":
+                stack.append(c)
+            else:
+                # No opening bracket to match
+                # Checks whether the stack is empty before trying to remove (pop) an element from it.
+                if not stack:
+                    return False
+                
+                top = stack.pop()
+                
+                # Check matching pairs
+                if (c == ')' and top != '(') or \
+                   (c == '}' and top != '{') or \
+                   (c == ']' and top != '['):
+                    return False
+        
+        # Valid only if no unmatched brackets remain
+        return len(stack) == 0
